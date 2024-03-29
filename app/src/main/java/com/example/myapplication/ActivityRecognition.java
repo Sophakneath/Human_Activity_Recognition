11f8486b3ef57913490ac52d826f4c29e37c4c0e
Lengry Sophakneath <sophakneath08@snu.ac.kr>
2024-03-29 17:33:19 +0900

    implement basic functions

--- app/src/main/java/com/example/myapplication/ActivityRecognition.java
+++ app/src/main/java/com/example/myapplication/ActivityRecognition.java
@@ -1,24 +1,189 @@
 package com.example.myapplication;
 
-import android.content.Intent;
+import static com.example.myapplication.utils.Constant.REQUEST_CODE_PERMISSIONS;
+import static com.example.myapplication.utils.Constant.REQUIRED_PERMISSIONS;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.ContentValues;
+import android.content.pm.PackageManager;
+import android.media.MediaScannerConnection;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.util.Size;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ImageAnalysis;
+import androidx.camera.core.ImageCapture;
+import androidx.camera.core.ImageCaptureException;
+import androidx.camera.core.ImageProxy;
+import androidx.camera.core.Preview;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.camera.view.PreviewView;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.lifecycle.LifecycleOwner;
+
+import com.github.mikephil.charting.charts.LineChart;
+import com.google.common.util.concurrent.ListenableFuture;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Objects;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 public class ActivityRecognition extends AppCompatActivity {
+
+
+    private static final String TAG = ActivityRecognition.class.getName();
+    private boolean checkPermissions() {
+        for(String permission: REQUIRED_PERMISSIONS){
+            int permissionState = ActivityCompat.checkSelfPermission(this, permission);
+            if(permissionState != PackageManager.PERMISSION_GRANTED) return false;
+        }
+        return true;
+    }
+
+    private void requestPermission() {
+        ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (grantResults.length <= 0) Log.i(TAG, "User interaction was cancelled");
+            else if (grantResults[0] == PackageManager.PERMISSION_GRANTED) startCamera();
+            else Log.i(TAG, "Permission Denied");
+        }
+    }
+
+    private void startCamera() {
+        final ListenableFuture<ProcessCameraProvider> cameraProviderFuture = ProcessCameraProvider.getInstance(this);
+        cameraProviderFuture.addListener(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                        bindUseCases(cameraProvider);
+                    }
+                } catch (ExecutionException | InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }, ContextCompat.getMainExecutor(this));
+    }
+
+    private void takePhoto(ImageCapture imageCapture, Executor mCameraExecutor) {
+        ContentValues contentValues = new ContentValues();
+        long currentTime = System.currentTimeMillis();
+
+        contentValues.put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg");
+        contentValues.put(MediaStore.Images.Media.DISPLAY_NAME, new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date()));
+        contentValues.put(MediaStore.Images.Media.DATE_TAKEN, currentTime);
+        contentValues.put(MediaStore.Images.Media.RELATIVE_PATH, Environment.DIRECTORY_PICTURES);
+
+        ImageCapture.OutputFileOptions outputFileOptions = new ImageCapture.OutputFileOptions.Builder(getContentResolver(),
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues).build();
+
+        imageCapture.takePicture(outputFileOptions, mCameraExecutor, new ImageCapture.OnImageSavedCallback() {
+            @Override
+            public void onImageSaved(@NonNull ImageCapture.OutputFileResults outputFileResults) {
+                Uri outputUri = outputFileResults.getSavedUri();
+                MediaScannerConnection.scanFile(ActivityRecognition.this,
+                        new String[]{outputUri.getPath()}, null,
+                        new MediaScannerConnection.OnScanCompletedListener() {
+                            @Override
+                            public void onScanCompleted(String path, Uri uri) {
+                                Toast toast = Toast.makeText(ActivityRecognition.this, "Image Saved.", Toast.LENGTH_LONG);
+                                toast.show();
+                            }
+                        });
+                Log.d(TAG, "Camera Saved");
+            }
+
+            @Override
+            public void onError(@NonNull ImageCaptureException exception) {
+
+            }
+        }) ;
+    }
+
+    private void imageAnalysis(Executor mCameraExecutor) {
+        ImageAnalysis imageAnalysis = new ImageAnalysis.Builder()
+                .setTargetResolution(new Size(1280, 720)).setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST).build();
+
+        imageAnalysis.setAnalyzer(mCameraExecutor, new ImageAnalysis.Analyzer() {
+            @Override
+            public void analyze(@NonNull ImageProxy image) {
+                int rotationDegrees = image.getImageInfo().getRotationDegrees();
+
+                //coding here
+            }
+        });
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.R)
+    void bindUseCases(@NonNull ProcessCameraProvider cameraProvider) {
+        CameraSelector cameraSelector = new CameraSelector.Builder().requireLensFacing(CameraSelector.LENS_FACING_BACK).build();
+        Executor mCameraExecutor = Executors.newSingleThreadExecutor();
+
+        ///////////////////////////////////// Preview Use Case ////////////////////////////////////
+        //build preview to use case
+        Preview preview = new Preview.Builder().build();
+
+        //get preview output surface
+        PreviewView mPreviewView = (PreviewView) findViewById(R.id.preview);
+        preview.setSurfaceProvider(mPreviewView.getSurfaceProvider());
+
+        cameraProvider.unbindAll();
+
+        //bind use cases to camera
+        ImageCapture imageCapture = new ImageCapture.Builder()
+                .setTargetRotation(Objects.requireNonNull(this.getDisplay()).getRotation()).build();
+        cameraProvider.bindToLifecycle((LifecycleOwner) this, cameraSelector, preview, imageCapture);
+
+        ///////////////////////////////////////////////////////////////////////////////////////////
+
+
+        ///////////////////////////////// Image Capture Use Case /////////////////////////////////
+
+        takePhoto(imageCapture, mCameraExecutor);
+
+        ///////////////////////////////////////////////////////////////////////////////////////////
+
+
+        ///////////////////////////////// Image Analysis Use Case /////////////////////////////////
+
+        imageAnalysis(mCameraExecutor);
+
+        ///////////////////////////////////////////////////////////////////////////////////////////
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_recognition);
+        setContentView(R.layout.activity_obj_classification);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -30,6 +195,18 @@ public class ActivityRecognition extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 ActivityRecognition.this.finish();
+            }
+        });
+
+        Button start = (Button) findViewById(R.id.start);
+        TextView result = (TextView) findViewById(R.id.result);
+        ImageView imageResult = (ImageView) findViewById(R.id.imageResult);
+        TextView confident = (TextView) findViewById(R.id.confident);
+        LineChart lineChart = (LineChart) findViewById(R.id.graph);
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startCamera();
             }
         });
     }
